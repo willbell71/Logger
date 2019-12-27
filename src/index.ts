8d82346c974f742f1573059cb641f7c55f1d34f5
willbell71 <williambell@bustermachine3.home>
2019-12-27 19:49:37 +0000

    Bugfix: added indexes

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import './elogger-level';
+import './ilog-line';
+import './ilogger';
+
+import './logger';
+import './log-line-console-log';
+import './log-line-console-warn';
+import './log-line-console-error';
